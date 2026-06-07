6b5a57555905043f0955f0229aec4329a20bc3f0
Ezequiel Sanchez <ezche3819@gmail.com>
2026-06-07 15:20:13 -0300

    Mejoras en la interfaz

--- script.js
+++ script.js
@@ -708,9 +708,9 @@ En el programa, cuando su secreto sale poco a poco a la luz, siente culpa y asum
         /* ── SLIDE 1: 15 imágenes (la última es la paleta de colores) ── */
         slide1: [
             /* ⬇️  Reemplazá cada "" con la ruta de tu imagen, ej: "img/mood/foto1.jpg" */
-            "img/moodboard/1/001.webp", "img/moodboard/1/002.webp", "img/moodboard/1/003.webp", "img/moodboard/1/004.webp", "img/moodboard/1/005.webp",
-            "img/moodboard/1/006.webp", "img/moodboard/1/007.webp", "img/moodboard/1/008.webp", "img/moodboard/1/009.webp", "img/moodboard/1/010.webp",
-            "img/moodboard/1/011.webp", "img/moodboard/1/012.webp", "img/moodboard/1/013.webp", "img/moodboard/1/014.webp", "img/paleta.jpeg"  /* última = paleta */
+            "img/moodboard/01/001.webp", "img/moodboard/01/002.webp", "img/moodboard/01/003.webp", "img/moodboard/01/004.webp", "img/moodboard/01/005.webp",
+            "img/moodboard/01/006.webp", "img/moodboard/01/007.webp", "img/moodboard/01/008.webp", "img/moodboard/01/009.webp", "img/moodboard/01/010.webp",
+            "img/moodboard/01/011.webp", "img/moodboard/01/012.webp", "img/moodboard/01/013.webp", "img/moodboard/01/014.webp", "img/paleta.jpeg"  /* última = paleta */
         ],
         /* ── SLIDE 2: 11 imágenes (la última es la paleta de colores) ── */
         slide2: [
