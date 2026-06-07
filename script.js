80692f1d69809a8b790d9162ddfe9f366ad426a3
Ezequiel Sanchez <ezche3819@gmail.com>
2026-06-07 14:59:03 -0300

    Mejoras en la interfaz

--- script.js
+++ script.js
@@ -710,7 +710,7 @@ En el programa, cuando su secreto sale poco a poco a la luz, siente culpa y asum
             /* ⬇️  Reemplazá cada "" con la ruta de tu imagen, ej: "img/mood/foto1.jpg" */
             "img/moodboard/001.webp", "img/moodboard/002.webp", "img/moodboard/003.webp", "img/moodboard/004.webp", "img/moodboard/005.webp",
             "img/moodboard/006.webp", "img/moodboard/007.webp", "img/moodboard/008.webp", "img/moodboard/009.webp", "img/moodboard/010.webp",
-            "img/moodboard/011.webp", "img/moodboard/012.webp", "img/moodboard/013.webp", "img/moodboard/014.webp", "img/paleta1.jpg"  /* última = paleta */
+            "img/moodboard/011.webp", "img/moodboard/012.webp", "img/moodboard/013.webp", "img/moodboard/014.webp", "img/moodboard/paleta.jpeg"  /* última = paleta */
         ],
         /* ── SLIDE 2: 11 imágenes (la última es la paleta de colores) ── */
         slide2: [
